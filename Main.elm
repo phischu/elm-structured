a049738b49bfb49223f2f46d6c1b3ac773dce2b9
phischu <pschuster@uni-koblenz.de>
2014-06-12 17:21:07 +0200

    refactor

--- Main.elm
+++ Main.elm
@@ -9,13 +9,16 @@ inputexpr = input {selected=1,expr=testexpr}
 testexpr : Expr
 testexpr = Plus 0 (Plus 1 (Value 2 5) (Value 3 4)) (Value 4 4)
 
-render : SelectedExpr -> Expr -> Element
-render original expr = case expr of
+render : SelectedExpr -> Element
+render sexpr = render' sexpr sexpr.expr
+
+render' : SelectedExpr -> Expr -> Element
+render' original expr = case expr of
     (Plus uid lhs rhs) -> flow right [
         keyword "(",
-        render original lhs,
+        render' original lhs,
         keyword "+",
-        render original rhs,
+        render' original rhs,
         keyword ")"]
         |> color (selectedColor (original.selected==uid))
         |> clickable inputexpr.handle {selected=uid,expr=original.expr}
@@ -32,4 +35,4 @@ selectedColor selected = if selected then lightYellow else transparent
 transparent : Color
 transparent = rgba 255 255 255 0
 
-main = lift (\o -> render o o.expr) inputexpr.signal
+main = lift render inputexpr.signal
