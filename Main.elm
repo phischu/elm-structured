16a45a40c36706f04de29a891235c57ed3e44369
phischu <pschuster@uni-koblenz.de>
2014-06-12 16:47:12 +0200

    refaftoring

--- Main.elm
+++ Main.elm
@@ -1,21 +1,26 @@
-import Mouse
+import Graphics.Input (Input,input)
 
 type Selected = Bool
 data Expr = Plus Selected Expr Expr | Value Selected Int
 
+exprinput : Input Expr
+exprinput = input testexpr
+
 testexpr : Expr
-testexpr = Plus False (Plus True (Value False 5) (Value False 4)) (Value False 4)
+testexpr = Plus False (Plus False (Value False 5) (Value False 4)) (Value True 4)
 
 render : Expr -> Element
 render expr = case expr of
-    (Plus selected lhs rhs) -> color (selectedColor selected) (flow right [keyword "(",render lhs,keyword "+",render rhs,keyword ")"])
-    (Value selected v) -> color (selectedColor selected) (leftAligned (toText (show v)))
+    (Plus selected lhs rhs) -> flow right [keyword "(",render lhs,keyword "+",render rhs,keyword ")"]
+        |> color (selectedColor selected)
+    (Value selected v) -> leftAligned (toText (show v))
+        |> color (selectedColor selected)
 
 keyword : String -> Element
 keyword s = leftAligned (toText s)
 
 selectedColor : Selected -> Color
-selectedColor selected = if selected then red else transparent
+selectedColor selected = if selected then lightYellow else transparent
 
 transparent : Color
 transparent = rgba 255 255 255 0
