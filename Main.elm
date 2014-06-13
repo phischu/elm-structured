c76c56853e648443163a3316f4505e5f1d997a3c
phischu <pschuster@uni-koblenz.de>
2014-06-13 10:12:31 +0200

    fold version

--- Main.elm
+++ Main.elm
@@ -1,51 +1,57 @@
 import Graphics.Input (Input,input,clickable)
 
 type UID = Int
-data Expr = Plus UID Expr Expr | Value UID Int
-type SelectedExpr = {selected : UID,expr : Expr}
+data ExprF a = PlusF a a | ValueF Int
+data Expr = Expr UID (ExprF Expr)
+type SelectedExpr = {selecteduid : UID,expr : Expr}
+
+type Fold a = {
+    selected : UID -> ExprF a -> a,
+    standard : UID -> ExprF a -> a}
+
+runFold : Fold a -> SelectedExpr -> a
+runFold fold {selecteduid,expr} = case expr of
+    Expr uid exprf ->
+        (if selecteduid == uid then fold.selected uid else fold.standard uid) (
+            case exprf of
+                PlusF lhs rhs ->
+                    PlusF
+                        (runFold fold {selecteduid = selecteduid,expr = lhs})
+                        (runFold fold {selecteduid = selecteduid,expr = rhs})
+                ValueF v ->
+                    ValueF v)
 
 inputexpr : Input SelectedExpr
-inputexpr = input {selected=1,expr=testexpr}
+inputexpr = input {selecteduid=1,expr=testexpr}
+
+plus : UID -> Expr -> Expr -> Expr
+plus uid lhs rhs = Expr uid (PlusF lhs rhs)
+
+value : UID -> Int -> Expr
+value uid v = Expr uid (ValueF v)
 
 testexpr : Expr
-testexpr = Plus 0 (Plus 1 (Value 2 5) (Value 3 4)) (Value 4 4)
+testexpr = plus 0 (plus 1 (value 2 5) (value 3 4)) (value 4 4)
 
-render : SelectedExpr -> Element
-render sexpr = render' sexpr sexpr.expr
+render : SelectedExpr -> Fold Element
+render sexpr = {selected = renderSelected sexpr,standard = renderStandard sexpr}
 
-render' : SelectedExpr -> Expr -> Element
-render' original expr = case expr of
-    (Plus uid lhs rhs) -> flow right [
+renderSelected : SelectedExpr -> UID -> ExprF Element -> Element
+renderSelected original uid exprf = color lightYellow (renderStandard original uid exprf)
+
+renderStandard : SelectedExpr -> UID -> ExprF Element -> Element
+renderStandard original uid exprf = case exprf of
+    PlusF lhs rhs -> flow right [
         keyword "(",
-        render' original lhs,
+        lhs,
         keyword "+",
-        render' original rhs,
+        rhs,
         keyword ")"]
-        |> color (selectedColor (original.selected==uid))
-        |> clickable inputexpr.handle {selected=uid,expr=original.expr}
-    (Value uid v) -> leftAligned (toText (show v))
-        |> color (selectedColor (original.selected==uid))
-        |> clickable inputexpr.handle {selected=uid,expr=original.expr}
+        |> clickable inputexpr.handle {selecteduid=uid,expr=original.expr}
+    (ValueF v) -> leftAligned (toText (show v))
+        |> clickable inputexpr.handle {selecteduid=uid,expr=original.expr}
 
 keyword : String -> Element
 keyword s = leftAligned (toText s)
 
-selectedColor : Bool -> Color
-selectedColor selected = if selected then lightYellow else transparent
-
-transparent : Color
-transparent = rgba 255 255 255 0
-
-type Rewrites = [Rewrite]
-type Rewrite = {names : [String],sexpr : SelectedExpr}
-
-rewrites : SelectedExpr -> Rewrites
-rewrites _ = [{names=["id"],sexpr={selected = 1,expr = testexpr}}]
-
-renderRewrites : Rewrites -> Element
-renderRewrites rewrites = flow down (map renderRewrite rewrites)
-
-renderRewrite : Rewrite -> Element
-renderRewrite rewrite = clickable inputexpr.handle rewrite.sexpr (keyword (show rewrite.names))
-
-main = lift (\sexpr -> render sexpr `beside` renderRewrites (rewrites sexpr)) inputexpr.signal
+main = lift (\sexpr -> runFold (render sexpr) sexpr) inputexpr.signal
